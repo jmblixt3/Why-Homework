42835b16cfde9c1ca04221fe60f48bf5eb543da4
jonathan <jonathan@jonathan-Satellite-A505>
2019-04-20 12:40:36 -0500

    URL fix

--- src/gui/BasicFXMLController.java
+++ src/gui/BasicFXMLController.java
@@ -13,86 +13,92 @@ import solve.Scan;
 
 public class BasicFXMLController {
 
-    @FXML
-    private Button loadHTML;
-    private Button saveVocab;
-    private Button stopWebDriver;
-    private Button startWebDriver;
-    @FXML
-    private TextField username;
-    @FXML
-    private PasswordField password;
-    @FXML
-    private TextField url;
-   
-    Loader load;
-    Driver driver;
-    
- // Add a public no-args constructor
-    
-    public BasicFXMLController() 
-    {
-    }
-    @FXML
-    public void initialize() {	
-        // TODO
-    	load = new Loader();
-    	driver = new Driver();
-    }
+	@FXML
+	private Button loadHTML;
+	private Button saveVocab;
+	private Button stopWebDriver;
+	private Button startWebDriver;
+	@FXML
+	private TextField username;
+	@FXML
+	private PasswordField password;
+	@FXML
+	private TextField url;
 
-    
-    @FXML
-    private void LoadHTML() 
-    {
-    	load.LoadHTML();
-    }
-    @FXML
-    private void SaveVocab()
-    {
-    	load.SaveVocab();
-    }
-    @FXML
-    private void LoadVocab()
-    {
-    	load.LoadVocab();
-    }
-    @FXML
-    private void StartHack() {
+	Loader load;
+	Driver driver;
+
+	// Add a public no-args constructor
+
+	public BasicFXMLController() {
+	}
+
+	@FXML
+	public void initialize() {
+		// TODO
+		load = new Loader();
+		driver = new Driver();
+	}
+
+	@FXML
+	private void LoadHTML() {
+		load.LoadHTML();
+	}
+
+	@FXML
+	private void SaveVocab() {
+		load.SaveVocab();
+	}
+
+	@FXML
+	private void LoadVocab() {
+		load.LoadVocab();
+	}
+
+	@FXML
+	private void StartHack() {
 		/*
-		 * try { driver.getDriver().get("google.com"); } catch (Exception e1) { // TODO
+		 * try { driver.getDriver().get("google.com"); } catch (Exception e1) {
 		 * Auto-generated catch block driver.start(); }
 		 */
-    	
-    	Scan hack = new Scan(url.getText(),username.getText(),password.getText(),driver.getDriver());
-    	
-    	Thread thread = new Thread(new Runnable() {
 
-    	    @Override
-    	    public void run() {
-    	    	System.out.println(hack.getcount());
-    	    	System.out.println(load.getQuestions().size());
-    	    	System.out.println(load.getQuestions().get(1));
-    	    	while(hack.getcount()<load.getQuestions().size()) {
-    	    		hack.Fill(load.getQuestions(), load.getAnswers(),driver.getDriver());
-    	    	}
-    	    }
-    	    
-    	});
+		try {
+			driver.getDriver().get(url.getText());
+			Scan hack = new Scan(url.getText(), username.getText(), password.getText(), driver.getDriver());
+			Thread thread = new Thread(new Runnable() {
 
-    	thread.start();
+				@Override
+				public void run() {
+					System.out.println(hack.getcount());
+					System.out.println(load.getQuestions().size());
+					System.out.println(load.getQuestions().get(1));
+					while (hack.getcount() < load.getQuestions().size()) {
+						hack.Fill(load.getQuestions(), load.getAnswers(), driver.getDriver());
+					}
+				}
 
-    }
-    @FXML
-    public void StartWebDriver() {
-    	driver.start();
-    }
-    @FXML
-    public void StopWebDriver() {
-    	driver.stop();
-    }
-    @FXML
-    public void Quit() {
-    	System.out.println("CLOSING");
-        System.exit(0);
-    }
+			});
+			thread.start();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+	@FXML
+	public void StartWebDriver() {
+		driver.start();
+	}
+
+	@FXML
+	public void StopWebDriver() {
+		driver.stop();
+	}
+
+	@FXML
+	public void Quit() {
+		System.out.println("CLOSING");
+		System.exit(0);
+	}
 }
